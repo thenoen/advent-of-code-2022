b8f0c584511ea8186e0c33ccaa8a08eff6c77e85
marek <kordikmarek@gmail.com>
2022-12-01 21:07:13 +0100

    project setup

---
+++ src/main/java/sk/thenoen/aoc/Main.java
@@ -0,0 +1,10 @@
+package sk.thenoen.aoc;
+
+public class Main {
+
+
+	public void solve() {
+		System.out.println("running");
+	}
+
+}
---
+++ src/test/java/sk/thenoen/aoc/MainTest.java
@@ -0,0 +1,15 @@
+package sk.thenoen.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+	@Test
+	public void test() {
+		new Main().solve();
+	}
+
+
+}
